7888766c8ddb6b94d5e68b068897e40c91274c2d
pokkihju <cyril.rideau@epita.fr>
2019-02-02 14:45:02 +0100

    Corrected compilation errors. (5)
    Signed-off-by: pokkihju <cyril.rideau@epita.fr>

--- src/filesystem/build_metatree.c
+++ src/filesystem/build_metatree.c
@@ -1,3 +1,11 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <err.h>
 #include "build_metatree.h"
 
 struct meta_tree *sub_build_mti(char *path)
