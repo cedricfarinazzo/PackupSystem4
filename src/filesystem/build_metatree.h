c761876fd61f1fcabbdaeba175344e2df447b727
pokkihju <cyril.rideau@epita.fr>
2019-02-02 11:48:06 +0100

    Added header of build_metatree.
    Signed-off-by: pokkihju <cyril.rideau@epita.fr>

---
+++ src/filesystem/build_metatree.h
@@ -0,0 +1,45 @@
+#ifndef _SRC_FILESYSTEM_BUILD_METATREE_H
+#define _SRC_FILESYSTEM_BUILD_METATREE_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+
+struct meta_data
+{
+    char *path;
+    struct stat fs;
+}
+
+struct meta_tree
+{
+    neta_data data;
+    meta_tree *son;
+    meta_tree *sibling;
+}
+
+/*build_metatree: creates a tree with all metadata\
+                  It takes the form of a tree as bintree\
+                  each node represents a file, each left son\
+                  is a son of the directory and each\
+                  right son is a sibling of the file or\
+                  directory.\
+                  the tree has a sentinel that is the root.
+  char *path: path to the directory that has to be represented\
+              as a tree.
+  return: a pointer to the allocated memory for the tree.
+*/
+
+struct meta_tree *build_metatree(char *path);
+
+/*free_meta_tree: free allocated memory for meta_tree struct and all it's\
+                  sons and sibings.\
+  struct meta_tre *tree: the tree that has to be freed;
+  return: int indicating wether free was a success or not.
+*/
+
+int free_meta_tree(struct meta_tree *tree);
+
+#endif //_SRC_FILESYSTEM_BUILD_METATREE_H
