01743bf68790d49956314cc85ef15b6e4f76e01c
Remi Maubanc <remi.maubanc@epita.fr>
2019-02-13 11:33:31 +0100

    Un piti commit
    Signed-off-by: Remi Maubanc <remi.maubanc@epita.fr>

--- src/compression/huffman.c
+++ src/compression/huffman.c
@@ -1,3 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <err.h>
+#include "bintree.h"
 
+
+//Compression
+
+void buildFrequenceList(char dataIN[])
+{
+    //char resultList
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        errx(4, "Argument number invalid !");
+    }
+    char *dataIN = argv[1];
+    return 0;
+}
