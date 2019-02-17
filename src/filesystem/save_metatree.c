c557b271cb1ce96beb342ed052501aaaf9cf1223
pokkihju <cyril.rideau@epita.fr>
2019-02-17 11:24:17 +0100

    save metatree in file done. yet to be tested.
    Signed-off-by: pokkihju <cyril.rideau@epita.fr>

--- src/filesystem/save_metatree.c
+++ src/filesystem/save_metatree.c
@@ -2,20 +2,33 @@
 
 void FS_save_path(char *path, FILE *save)
 {
-    path = path;
-    save = save;
+    long length = 1;
+    length += strlen(path);
+    e = fwrite(&length, sizeof(char), sizeof(long), save);
+    if (e < sizeof(long))
+        err(33, "FS_save_path failure. fwrite failure (1).");
+    e = fwrite(path, sizeof(char), length, save);
+    if (e < length)
+        err(33, "FS_save_path failure. fwrite failure (2).");
 }
 
 void FS_save_stats(struct stat fs, FILE *save)
 {
-    fs = fs;
-    save = save;
+    int e = fwrite(&fs, 1, sizeof(struct stat), save);
+    if (e < sizeof(struct stat))
+        err(33, "FS_save_stats failure. fwrite failure.");
 }
 
 void FS_save_inheritance(struct meta_tree *tree, FILE *save)
 {
-    tree = tree;
-    save = save;
+    char inheritance = 0;
+    if (tree->son)
+        inheritance |= FILESYSTEM_TREE_HAS_SON;
+    if (tree->sibling)
+        inheritance |= FILESYSTEM_TREE_HAS_SIBLING;
+    int e = fwrite(&inheritance, sizeof(char), 1, save);
+    if (e < 1)
+        err(33, "FS_save_inheritance failure. fwrite failure.");
 }
 
 void FS_save_data(struct meta_data *data, FILE *save)
@@ -41,6 +54,24 @@ void FILESYSTEM_save_metatree(struct meta_tree *tree, char *path)
     FILE *file = fopen(path, "w");
     FS_save_tree(file, tree);
     fclose(file);
+}
+
+char *FS_restore_path(FILE *file)
+{
+    file = file;
+    return NULL;
+}
+
+struct stat FS_restore_stats(FILE *file)
+{
+    file = file;
+    return NULL;
+}
+
+struct meta_data *FS_restore_data(FILE *file)
+{
+    file = file;
+    return NULL;
 }
 
 struct meta_tree *FS_restore_tree(FILE *file)
