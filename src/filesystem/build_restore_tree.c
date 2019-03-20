a63904ed2fd73fc7deccc4a7679a9270bd3b1014
Cyril Rideau <cyril.rideau@epita.fr>
2019-03-20 14:16:15 +0100

    added structure to c file build_restore_tree
    Signed-off-by: Cyril Rideau <cyril.rideau@epita.fr>

---
+++ src/filesystem/build_restore_tree.c
@@ -0,0 +1,19 @@
+#include "build_restore_tree.h"
+
+struct restore_tree *__compare_and_build(struct restore_tree *rtree, struct meta_tree *tree)
+{
+    //TODO
+    return NULL;
+}
+
+struct restore_tree *compare_and_build(struct restore_tree *rtree, struct meta_tree *tree)
+{
+    //TODO
+    return NULL;
+}
+
+struct restore_tree *FILESYSTEM_build_restore_tree(DIR *save_dir)
+{
+    //TODO
+    return NULL;
+}
