ea4c9f879ec17c2f62a946bd3a211d3471af72a4
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-03-22 23:03:08 +0100

    aes: add doc and readme
    Signed-off-by: cedric.farinazzo <cedric.farinazzo@epita.fr>

--- src/encryption/aes/hashpass.h
+++ src/encryption/aes/hashpass.h
@@ -5,6 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* AES_keyFromPass: generate an aes key from password
+ * pass: char*: a password
+ * len: size_t: lenght of pass
+ * return: unsigned char*: an aes key of 16 byte ended by null character
+ */
 unsigned char *AES_keyFromPass(char *pass, size_t len);
 
 #endif /* _SRC_ENCRYPTION_AES_HASSPASS_H__ */
