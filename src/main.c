a3b723c493a5aaf80e61fb3339ed521b2f7d8dca
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-03-22 01:09:11 +0100

    update main.c
    Signed-off-by: cedric.farinazzo <cedric.farinazzo@epita.fr>

--- src/main.c
+++ src/main.c
@@ -26,11 +26,10 @@
 #include "filesystem/save_metatree.h"
 
 #define VERSION "1.0"
-#define DATE "2018-02"
-#define TYPE "release"
+#define DATE "2018-03"
+#define TYPE "dev"
 
 // TO MOVE IN FILESYSTEM
-/*
 void print_tree(struct meta_tree *tree, int indent)
 {
     char indents[indent + 1];
@@ -53,7 +52,6 @@ void print_tree(struct meta_tree *tree, int indent)
         }
     }
 }
-*/
 
 void print_ascii(unsigned char *a)
 {
@@ -63,140 +61,7 @@ void print_ascii(unsigned char *a)
 
 int main(int argc, char *argv[])
 {
-   
-
-    unsigned char text[] = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-    unsigned char key[] = "01G345a.89sbhdef";
- 
-    printf("text(%ld): %s   | key: %s\n", strlen((char*)text), text, key);
-
-    unsigned char *output = NULL;
-    unsigned char *decrypt = NULL;
-
-    struct AES_ctx *ctx = AES_init(key, strlen((char*)key));
-
-    size_t outlen = AES_encrypt(ctx, text, strlen((char*)text), &output);
-    printf("\nencrypted text (%ld): %s  |   ", outlen, output);
-    print_ascii(output); printf("\n");
-
-    size_t delen = AES_decrypt(ctx, output, outlen, &decrypt);
-    printf("\n\ndecrypted text (%ld): %s  |   ", delen, decrypt);
-    print_ascii(decrypt); printf("\n");
-
-    free(output);
-    free(decrypt);
-    AES_ctx_free(ctx);
-
-    return EXIT_SUCCESS;
-    /*
-    uint8_t data[16] = 
-    {
-        0x04, 0xe0, 0x48, 0x28,
-        0x66, 0xcb, 0xf8, 0x06,
-        0x81, 0x19, 0xd3, 0x26,
-        0xe5, 0x9a, 0x7a, 0x4c,
-    };
-    uint8_t key[16] = 
-    {
-        0xa0, 0x88, 0x23, 0x2a,
-        0xfa, 0x54, 0xa3, 0x6c,
-        0xfe, 0x2c, 0x39, 0x76,
-        0x17, 0xb1, 0x39, 0x05,
-    };
-    uint8_t exp[16] = 
-    {
-        0xa4, 0x68, 0x6b, 0x02,
-        0x9c, 0x9f, 0x5b, 0x6a,
-        0x7f, 0x35, 0xea, 0x50,
-        0xf2, 0x2b, 0x43, 0x49,
-    };
-    struct AES_matrix *mat = AES_matrix_init();
-    AES_matrix_feed(mat, data);
-
-    struct AES_matrix *expm = AES_matrix_init();
-    AES_matrix_feed(expm, exp);
-    
-    struct AES_matrix *keym = AES_matrix_init();
-    AES_matrix_feed(keym, key);
-    
-    struct AES_matrix *r = AES_matrix_addRoundKey(mat, keym);
-    
-    printf("%d", AES_matrix_areEqual(r, expm));
-
-    AES_matrix_free(r);
-   
-    AES_matrix_free(mat);
-    AES_matrix_free(keym);
-    AES_matrix_free(expm);
-
-    return EXIT_SUCCESS;
-    uint8_t data[16] = 
-    {
-        0xdb, 0xf2, 0x01, 0x2d,
-        0x13, 0x0a, 0x01, 0x26,
-        0x53, 0x22, 0x01, 0x31,
-        0x45, 0x5c, 0x01, 0x4c,
-
-    };
-    uint8_t exp[16] = 
-    {
-        0x8e, 0x9f, 0x01, 0x4d,
-        0x4d, 0xdc, 0x01, 0x7e,
-        0xa1, 0x58, 0x01, 0xbd,
-        0xbc, 0x9d, 0x01, 0xf8,
-
-    };
-    struct AES_matrix *mat = AES_matrix_init();
-    AES_matrix_feed(mat, data);
-    printf("\n== Mat \n");
-    AES_matrix_printfhex(mat);
-
-    struct AES_matrix *expm = AES_matrix_init();
-    AES_matrix_feed(expm, exp);
-    
-    
-    struct AES_matrix *mi = AES_matrix_mixColumns(mat);
-    printf("\n== MIX COLUMNS \n");
-    AES_matrix_printfhex(mi);
-    
-    printf("%d", AES_matrix_areEqual(mi, expm));
-
-    AES_matrix_free(mi);
-   
-    AES_matrix_free(mat);
-    AES_matrix_free(expm);
-
-
-
-
-
-    return EXIT_SUCCESS;
-    
     srand(time(NULL));
-
-    unsigned char *text = (unsigned char*)argv[1];
-    unsigned char *key = (unsigned char*)argv[2];
-
-    printf("text: %s   | key: %s\n", (char*)text, (char*)key);
-
-    unsigned char *output = NULL;
-    unsigned char *decrypt = NULL;
-
-    AES_encrypt(text, key, &output);
-    printf("\n encrypted text: ");
-    printf("\n%s\n    \n", (char*)output);
-    print_ascii(output); printf("\n");
-
-    AES_decrypt(output, key, &decrypt);
-    printf("\n decrypted text: \n%s\n    \n", (char*)decrypt);
-    print_ascii(decrypt); printf("\n");
-
-    free(output);
-    free(decrypt);
-
-
-    return EXIT_SUCCESS;
-   
     if (argc == 1)
         return interface(argc, argv);
 
@@ -248,7 +113,7 @@ int main(int argc, char *argv[])
 
             ROTN_encrypt(text, key);
             printf("encrypted text: %s  |   ", text);
-            print_ascii(text); printf("\n");
+            print_ascii((unsigned char*)text); printf("\n");
 
             ROTN_decrypt(text, key);
             printf("decrypted text: %s\n", text);
@@ -263,7 +128,7 @@ int main(int argc, char *argv[])
 
             VIGENERE_encrypt(text, key);
             printf("encrypted text: %s  |   ", text);
-            print_ascii(text); printf("\n");
+            print_ascii((unsigned char*)text); printf("\n");
 
             VIGENERE_decrypt(text, key);
             printf("decrypted text: %s\n", text);
@@ -271,25 +136,27 @@ int main(int argc, char *argv[])
 
         if (strcmp("aes", argv[1]) == 0)
         {
-            char *text = argv[2];
-            char *key = argv[3];
+            unsigned char *text = (unsigned char*)argv[2];
+            unsigned char *key = (unsigned char*)argv[3];
 
-            printf("text: %s   | key: %s\n", text, key);
+            printf("text(%ld): %s   | key: %s\n", strlen((char*)text), text, key);
 
-            char *output = NULL;
-            char *decrypt = NULL;
+            unsigned char *output = NULL;
+            unsigned char *decrypt = NULL;
 
-            AES_encrypt(text, key, &output);
-            printf("encrypted text: %s  |   ", output);
+            struct AES_ctx *ctx = AES_init(key, strlen((char*)key));
+
+            size_t outlen = AES_encrypt(ctx, text, strlen((char*)text), &output);
+            printf("\nencrypted text (%ld): %s  |   ", outlen, output);
             print_ascii(output); printf("\n");
 
-            AES_decrypt(output, key, &decrypt);
-            printf("encrypted text: %s  |   ", decrypt);
+            size_t delen = AES_decrypt(ctx, output, outlen, &decrypt);
+            printf("\n\ndecrypted text (%ld): %s  |   ", delen, decrypt);
             print_ascii(decrypt); printf("\n");
 
             free(output);
             free(decrypt);
-
+            AES_ctx_free(ctx);
         }
     }
 
@@ -297,7 +164,7 @@ int main(int argc, char *argv[])
     {
         if (strcmp("rsa", argv[1]) == 0)
         {
-            char *text = argv[2];
+            char *  text = argv[2];
             size_t lentext = strlen(text);
             long lp = atol(argv[3]);
             long lq = atol(argv[4]);
@@ -348,7 +215,5 @@ int main(int argc, char *argv[])
 
         }
     }
-
     return EXIT_SUCCESS;
-    */
 }
