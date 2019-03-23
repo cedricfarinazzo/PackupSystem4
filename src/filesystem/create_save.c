0b40fa0159d767defaf84f50aaeed6437a18a92f
pokkihju <cyril.rideau@epita.fr>
2019-03-23 14:54:12 +0100

    I'm gonna delete everything if this doesn't work
    Signed-off-by: pokkihju <cyril.rideau@epita.fr>

--- src/filesystem/create_save.c
+++ src/filesystem/create_save.c
@@ -1,6 +1,6 @@
 #include "create_save.h"
 
-void FS_save_path(char *path, FILE *save)
+void CS_save_path(char *path, FILE *save)
 {
     long length = 1;
     length += strlen(path);
@@ -12,14 +12,14 @@ void FS_save_path(char *path, FILE *save)
         err(33, "FS_save_path failure. fwrite failure");
 }
 
-void FS_save_stats(struct stat fs, FILE *save)
+void CS_save_stats(struct stat fs, FILE *save)
 {
     int e = fwrite(&fs, 1, sizeof(struct stat), save);
     if (e < (long)sizeof(struct stat))
         err(33, "FS_save_path failure. fwrite failure");
 }
 
-void FS_save_inheritance(struct meta_tree *tree, FILE *save)
+void CS_save_inheritance(struct meta_tree *tree, FILE *save)
 {
     char inheritance = 0;
     if (tree->son)
@@ -31,28 +31,28 @@ void FS_save_inheritance(struct meta_tree *tree, FILE *save)
         err(33, "FS_save_inheritance failure. fwrite failed.");
 }
 
-void FS_save_data(struct meta_data *data, FILE *save)
+void CS_save_data(struct meta_data *data, FILE *save)
 {
-    FS_save_path(data->path, save);
-    FS_save_stats(data->fs, save);
+    CS_save_path(data->path, save);
+    CS_save_stats(data->fs, save);
 }
 
-void FS_save_file(char *path, FILE *save)
+void CS_save_file(char *path, FILE *save)
 {
     FILE *original = fopen(path, "w");
     FILESYSTEM_save_file_content(original, save);
     fclose(original);
 }
 
-void FS_save_tree(FILE *save, struct meta_tree *tree)
+void CS_save_tree(FILE *save, struct meta_tree *tree)
 {
-    FS_save_data(tree->data, save);
-    FS_save_inheritance(tree, save);
-    FS_save_file(tree->data->path, save);
+    CS_save_data(tree->data, save);
+    CS_save_inheritance(tree, save);
+    CS_save_file(tree->data->path, save);
     struct meta_tree *temp = tree->son;
     while (temp)
     {
-        FS_save_tree(save, temp);
+        CS_save_tree(save, temp);
         temp = temp->sibling;
     }
 }
@@ -61,7 +61,7 @@ void FILESYSTEM_create_save(char *path, char *savepath)
 {
     FILE *save = fopen(savepath, "w");
     struct meta_tree *tree = FILESYSTEM_build_metatree(path);
-    FS_save_tree(save, tree->son);
+    CS_save_tree(save, tree->son);
     FILESYSTEM_free_metatree(tree);
     fclose(save);
 }
