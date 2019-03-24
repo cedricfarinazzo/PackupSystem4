8018b4149cc2857ed92ca74a5d46e71def06585b
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-03-24 21:52:56 +0100

    rsa: add doc and readme
    Signed-off-by: cedric.farinazzo <cedric.farinazzo@epita.fr>

--- src/encryption/rsa/genkey.h
+++ src/encryption/rsa/genkey.h
@@ -16,10 +16,26 @@ struct RSA_privKey {
 } RSA_privatekey;
 
 
+/* RSA_generateKey: Generate private and public key
+ * keysize: unsigned long: the number of bit of the key. must be a power of 2 (ex: 1024, 2048 or 4096)
+ * privk: struct RSA_privKey*: a pointer on a struct RSA_privKey
+ * pubk: struct RSA_pubKey*: a pointer on a struct RSA_pubKey
+ * return: void
+ */
 void RSA_generateKey(unsigned long keysize, struct RSA_privKey **privk, struct RSA_pubKey **pubk);
 
+
+/* RSA_free_private_key: free the private key
+ * privk: struct RSA_privKey*: a private key generated RSA_generateKey
+ * return: void
+ */
 void RSA_free_private_key(struct RSA_privKey *privk);
 
+
+/* RSA_free_public_key: free the public key
+ * pubk: struct RSA_pubKey*: a public key generated RSA_generateKey
+ * return: void
+ */
 void RSA_free_public_key(struct RSA_pubKey *pubk);
 
 #endif /* _SRC_ENCRYPTION_RSA_GENKEY_H_ */
