bb1b6c390ce7f7547ee0d828064b60747a9d0678
Cyril Rideau <cyril.rideau@epita.fr>
2019-03-27 12:38:39 +0100

    commented and clarified save_metatree (part 1)
    Signed-off-by: Cyril Rideau <cyril.rideau@epita.fr>

--- src/filesystem/save_metatree.c
+++ src/filesystem/save_metatree.c
@@ -1,48 +1,115 @@
 #include "save_metatree.h"
 
+/*
+saves given path and its len in given file
+*/
+
 void FS_save_path(char *path, FILE *save)
 {
+    /*
+    calculating length counting terminating null byte
+    */
     long length = 1;
     length += strlen(path);
+
+    /*
+    writing said length
+    */
     int e = fwrite(&length, sizeof(char), sizeof(long), save);
     if (e < (long) sizeof(long))
         err(33, "FS_save_path failure. fwrite failure (1).");
+    
+    /*
+    writing path
+    */
     e = fwrite(path, sizeof(char), length, save);
     if (e < length)
         err(33, "FS_save_path failure. fwrite failure (2).");
 }
 
+/*
+saves given stat struct by writing it in given file
+*/
+
 void FS_save_stats(struct stat fs, FILE *save)
 {
+    /*
+    writing stats struct
+    */
     int e = fwrite(&fs, 1, sizeof(struct stat), save);
     if (e < (long) sizeof(struct stat))
         err(33, "FS_save_stats failure. fwrite failure.");
 }
 
+/*
+saves inheritance of tree by translating it in a byte and writing it in a file
+*/
+
 void FS_save_inheritance(struct meta_tree *tree, FILE *save)
 {
+    /*
+    initializing said byte to 0
+    */
     char inheritance = 0;
+
+    /*
+    if given tree has a son, indicate it in a byte
+    */
     if (tree->son)
         inheritance |= FILESYSTEM_TREE_HAS_SON;
+
+    /*
+    if given tree has a sibling, indicate it in a byte
+    */
     if (tree->sibling)
         inheritance |= FILESYSTEM_TREE_HAS_SIBLING;
+
+    /*
+    writing said byte in given file
+    */
     int e = fwrite(&inheritance, sizeof(char), 1, save);
     if (e < 1)
         err(33, "FS_save_inheritance failure. fwrite failure.");
 }
 
+/*
+saves meta_data struct in given file
+*/
+
 void FS_save_data(struct meta_data *data, FILE *save)
 {
+    /*
+    saves path first by calling save path function
+    */
     if (data->path)
         FS_save_path(data->path, save);
+
+    /*
+    saves stats then by calling save stat function
+    */
     FS_save_stats(data->fs, save);
 }
 
+/*
+save_tree function that saves tree by calling other functions
+*/
+
 void FS_save_tree(FILE *save, struct meta_tree *tree)
 {
+    /*
+    first save data
+    */
     if (tree->data)
         FS_save_data(tree->data, save);
+
+    /*
+    then save inheritance
+    */
     FS_save_inheritance(tree, save);
+
+    /*
+    then recursively save sons
+    */
     struct meta_tree *temp = tree->son;
     while (temp)
     {
@@ -53,6 +120,9 @@ void FS_save_tree(FILE *save, struct meta_tree *tree)
 
 void FILESYSTEM_save_metatree(struct meta_tree *tree, char *path)
 {
+    /*
+    opens file, call function to save tree then close file
+    */
     FILE *file = fopen(path, "w");
     FS_save_tree(file, tree->son);
     fclose(file);
