f19e5c0038fcd673fc578304cdd5345167233c01
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-03-31 00:41:32 +0100

    website login system
    Signed-off-by: cedric.farinazzo <cedric.farinazzo@epita.fr>

--- website/ps4/urls.py
+++ website/ps4/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 app_name = 'ps4'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home', views.index, name='home'),
-    path('download', views.download, name='download'),
-    path('report', views.report, name='report'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
-    path('account', views.account, name='account'),
-    path('logout', views.logout, name='logout'),
-    path('about', views.about, name='about'),
-    path('terms', views.terms, name='terms'),
+    path(r'', views.index, name='index'),
+    path(r'home/', views.index, name='home'),
+    path(r'download/', views.download, name='download'),
+    path(r'report/', views.report, name='report'),
+    path(r'login/', views.login, name='login'),
+    path(r'register/', views.register, name='register'),
+    path(r'account/', views.account, name='account'),
+    path(r'logout/', views.logout, name='logout'),
+    path(r'about/', views.about, name='about'),
+    path(r'terms/', views.terms, name='terms'),
 ]
--- website/ps4/views.py
+++ website/ps4/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.models import User
 from django.template import loader
 from django.http import HttpResponse
 
@@ -21,31 +25,62 @@ def download(request):
     return render(request, 'ps4/download.html', context)
 
 def login(request):
-    #if connected
-        # redirect to home
-
+    if request.user.is_authenticated:
+        return redirect('/')
     context = {}
+    if request.method == 'POST':
+        try:
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('/')
+            else:
+                context['error'] = 'Invalid username and/or password'
+        except:
+            context['error'] = 'An error occured. Please try again'
     return render(request, 'ps4/login.html', context)
 
 def register(request):
-    #if connected
-        # redirect to home
-
+    if request.user.is_authenticated:
+        return redirect('/')
+    
     context = {}
+    
+    if request.method == "POST":
+        try:
+            lastname = request.POST['name']
+            firtname = request.POST['firstname']
+            email = request.POST['email']
+            username = request.POST['username']
+            password = request.POST['password']
+            passcomf = request.POST['passcomfirm']
+            
+            if password != passcomf:
+                context['error'] = 'Passwords didn\'t match'
+            else:
+                u = User.objects.create_user(username, email, password)
+                u.last_name = lastname;
+                u.first_name = firtname
+                u.save()
+                auth_login(request, u)
+                return redirect('/')
+        except:
+            context['error'] = 'An error occured. Please try again'
     return render(request, 'ps4/register.html', context)
 
 def account(request):
-    #if NOT connected
-        # redirect to login
+    if not request.user.is_authenticated:
+        return redirect('/')
 
     context = {}
     return render(request, 'ps4/account.html', context)
 
 def logout(request):
-    #if connected
-        #LOGOUT
-    # redirect to home
-    pass
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect('/')
 
 def about(request):
 
