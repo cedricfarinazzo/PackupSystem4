8c466efc1a042e653ad82359c503d38634606a1c
pokkihju <cyril.rideau@epita.fr>
2019-03-30 11:00:37 +0100

    trying to correct missing files
    Signed-off-by: pokkihju <cyril.rideau@epita.fr>

--- src/filesystem/restore_save.h
+++ src/filesystem/restore_save.h
@@ -13,4 +13,6 @@ void FILESYSTEM_restore_save(char *save_dir);
 
 struct meta_tree *FILESYSTEM_SAVE_restore_metatree_from_save(FILE *file);
 
+void FILESYSTEM_restore_original_save(char *save);
+
 #endif // _SRC_FILESYSTEM_RESTORE_SAVE
