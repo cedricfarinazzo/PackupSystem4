de56c8b87bb8c59e9561f529f9db084ae477bbbf
pokkihju <cyril.rideau@epita.fr>
2019-04-01 20:42:27 +0200

    Looks like I made a mistake
    Signed-off-by: pokkihju <cyril.rideau@epita.fr>

--- src/filesystem/restore_save.c
+++ src/filesystem/restore_save.c
@@ -157,6 +157,7 @@ void RS_restore_from_meta_tree(struct meta_tree *tree, FILE *src)
 {
     if (tree->son == NULL)
     {
+        printf("restoring file: %s with offset: %ld\n", tree->data->path, tree->data->file_content);
         FILE *dst = fopen(tree->data->path, "w");
         RS_restore_content(src, tree->data->file_content, dst);
         fclose(dst);
