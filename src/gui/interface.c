b8aa5891fe2e6c42cc644ff25ffcac23c8fcdcf5
FredericCasier <frederic.casier@epita.fr>
2019-04-02 18:30:15 +0200

    Modification des fichiers pour gui dans develop, pour evité une grosse erreur de ma part
    Signed-off-by: FredericCasier <frederic.casier@epita.fr>

--- src/gui/interface.c
+++ src/gui/interface.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
-GtkBuilder *builder;
 GtkWidget *window;
 
 int interface(int argc,char *argv[])
 {
+    
+    GtkBuilder *builder;
+
     gtk_init(&argc,&argv);
 
     builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, "PS4_inter.glade",NULL);
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "main_windows"));
-
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
 
     gtk_builder_connect_signals(builder,NULL);
 
@@ -27,29 +28,49 @@ int interface(int argc,char *argv[])
     return EXIT_SUCCESS;
 }
 
-void on_Close_cliked()
-{
-    exit(0);
+void on_Close_clicked()
+{	
+     gtk_main_quit();
 }
 
-void on_Support_cliked()
+void on_huffman_clicked()
 {
-    printf("You cliked on Support\n");
+    printf("You clicked on Huffman\n");
 }
 
-void on_Compr_cliked()
+void on_filesystem_clicked()
 {
-    printf("You cliked on Compression\n");
+    printf("You clicked on Filesystem\n");
 }
 
-void on_Decompr_cliked()
+void on_backup_clicked()
 {
-    printf("You cliked on Decompression\n");
+    printf("You clicked on Backup\n");
 }
 
-void on_Chiffr_cliked()
+void on_rotn_clicked()
 {
-    printf("You cliked on Chiffrement\n");
+    printf("You clicked on Rotn\n");
+}
+
+void on_vigenere_clicked()
+{
+    printf("You clicked on Vigenere\n");
+}
+
+void on_rsa_clicked()
+{
+    printf("You clicked on RSA\n");
+}
+
+void on_aes_clicked()
+{
+    printf("You clicked on AES\n");
+}
+
+void on_aes_on_file_clicked()
+{
+    printf("You clicked on AES on file\n");
 }
 
 
