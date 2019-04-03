1c34b985dd369b7203b6e6037a03dd8f80efa29c
FredericCasier <frederic.casier@epita.fr>
2019-04-03 15:19:10 +0200

    fix interface.c
    Signed-off-by: FredericCasier <frederic.casier@epita.fr>

--- src/gui/interface.c
+++ src/gui/interface.c
@@ -3,6 +3,10 @@
 #include <gtk/gtk.h>
 
 GtkWidget *window;
+GtkBuilder *builder;
+
+GtkWidget *file_window;
+GtkWidget *input_window;
 
 int interface(int argc,char *argv[])
 {
@@ -15,6 +19,8 @@ int interface(int argc,char *argv[])
     gtk_builder_add_from_file(builder, "PS4_inter.glade",NULL);
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+    file_window = GTK_WIDGET(gtk_builder_get_object(builder, "file_main"));
+    input_window = GTK_WIDGET(gtk_builder_get_object(builder, "input_Huffman"));
 
     gtk_builder_connect_signals(builder,NULL);
 
@@ -35,25 +41,22 @@ void on_Close_clicked()
 
 void on_huffman_clicked()
 {
-    printf("You clicked on Huffman\n");
+     printf("You clicked on Huffman\n");
+     gtk_widget_show(input_window);
 }
 
 void on_filesystem_clicked()
 {
-    printf("You clicked on Filesystem\n");
+    printf("You clicked on File System\n");
+    gtk_widget_show(file_window);
 }
 
-void on_backup_clicked()
-{
-    printf("You clicked on Backup\n");
-}
-
-void on_rotn_clicked()
+void on_Rotn_clicked()
 {
     printf("You clicked on Rotn\n");
 }
 
-void on_vigenere_clicked()
+void on_Vigenere_clicked()
 {
     printf("You clicked on Vigenere\n");
 }
@@ -68,9 +71,24 @@ void on_aes_clicked()
     printf("You clicked on AES\n");
 }
 
-void on_aes_on_file_clicked()
+void on_aes_file_clicked()
 {
     printf("You clicked on AES on file\n");
 }
 
+void on_file_Validate_clicked()
+{
+    printf("You clicked on Validate\n");
+}
 
+void on_file_Close_clicked()
+{
+    printf("You clicked on Close\n");
+    gtk_widget_hide(file_window);
+}
+
+void on_input_huffman_close_clicked()
+{
+    printf("You clicked on Close\n");
+    gtk_widget_hide(input_window);
+}
