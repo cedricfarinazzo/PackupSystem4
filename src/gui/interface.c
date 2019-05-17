b4d8845afc62adb8e235fb99d235a56796a4056a
FredericCasier <frederic.casier@epita.fr>
2019-05-17 19:16:24 +0200

    update: interface.c ; strugling with the checkbox
    Signed-off-by: FredericCasier <frederic.casier@epita.fr>

--- src/gui/interface.c
+++ src/gui/interface.c
@@ -5,8 +5,16 @@
 GtkWidget *window;
 GtkBuilder *builder;
 
-GtkWidget *file_window;
-GtkWidget *input_window;
+GtkWidget *main_window;
+GtkWidget *make_backup;
+GtkWidget *ask_file_load;
+GtkWidget *ask_file_restore;
+GtkWidget *ask_file_create;
+
+int encrypt;
+int compr;
+int decrypt;
+int decompr;
 
 int interface(int argc,char *argv[])
 {
@@ -18,15 +26,17 @@ int interface(int argc,char *argv[])
     builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, "PS4_inter.glade",NULL);
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
-    file_window = GTK_WIDGET(gtk_builder_get_object(builder, "file_main"));
-    input_window = GTK_WIDGET(gtk_builder_get_object(builder, "input_Huffman"));
+    main_window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+    make_backup = GTK_WIDGET(gtk_builder_get_object(builder, "make_backup"));
+    ask_file_load = GTK_WIDGET(gtk_builder_get_object(builder, "ask_file_load"));
+    ask_file_create = GTK_WIDGET(gtk_builder_get_object(builder, "ask_file_create"));
+    ask_file_restore = GTK_WIDGET(gtk_builder_get_object(builder, "ask_file_restore"));
 
     gtk_builder_connect_signals(builder,NULL);
 
     g_object_unref(builder);
 
-    gtk_widget_show(window);
+    gtk_widget_show(main_window);
     gtk_main();
 
 
@@ -39,56 +49,63 @@ void on_Close_clicked()
      gtk_main_quit();
 }
 
-void on_huffman_clicked()
+void on_backup_clicked()
 {
-     printf("You clicked on Huffman\n");
-     gtk_widget_show(input_window);
+     printf("You clicked on Backup\n");
+     gtk_widget_show(make_backup);
+     gtk_widget_hide(main_window);
 }
 
-void on_filesystem_clicked()
+void on_restore_clicked()
 {
-    printf("You clicked on File System\n");
-    gtk_widget_show(file_window);
+    printf("You clicked on Restore\n");
+    gtk_widget_show(ask_file_restore);
+    gtk_widget_hide(main_window);
 }
 
-void on_Rotn_clicked()
+void on_create_clicked()
 {
-    printf("You clicked on Rotn\n");
+    printf("You clicked on Create\n");
+    gtk_widget_show(ask_file_create);
+    gtk_widget_hide(make_backup);
 }
 
-void on_Vigenere_clicked()
+void on_load_clicked()
 {
-    printf("You clicked on Vigenere\n");
+    printf("You clicked on Load\n");
+    gtk_widget_show(ask_file_load);
+    gtk_widget_hide(make_backup);
 }
 
-void on_rsa_clicked()
+void on_save_clicked()
 {
-    printf("You clicked on RSA\n");
+    //GtkToggleButton *decompr_toggle;
+    //GtkToggleButton *decrypt_toggle;
+
+    //decompr_toggle = GTK_Widget(gtk_builder_get_object(builder, "decompr"));
+    //decrypt_toggle = GTK_Widget(gtk_builder_get_object(builder, "decrypt"));
+
+
+    printf("You clicked on save\n");
+    //gboolean decrypt_state;
+    //gboolean decompr_state;
+
+    //decompr_state = gtk_toggle_button_get_active(decompr_toggle);
+    //decrypt_state = gtk_toggle_button_get_active(decrypt_toggle);
+
+    //if(decompr_state){decompr=1;}
+    //else{decompr=0;}
+
+    //if(decrypt_state){decrypt=1;}
+    //else{decrypt=0;}
 }
 
-void on_aes_clicked()
+void on_save_create_clicked()
 {
-    printf("You clicked on AES\n");
+    printf("You clicked on save1\n");
 }
 
-void on_aes_file_clicked()
+void on_restore_button_clicked()
 {
-    printf("You clicked on AES on file\n");
-}
-
-void on_file_Validate_clicked()
-{
-    printf("You clicked on Validate\n");
-}
-
-void on_file_Close_clicked()
-{
-    printf("You clicked on Close\n");
-    gtk_widget_hide(file_window);
-}
-
-void on_input_huffman_close_clicked()
-{
-    printf("You clicked on Close\n");
-    gtk_widget_hide(input_window);
+    printf("You clicked on Restore\n");
 }
