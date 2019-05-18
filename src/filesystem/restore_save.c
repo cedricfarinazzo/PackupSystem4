34c214c221afc765a380bbf25a8a410f367d6b86
pokkihju <cyril.rideau@epita.fr>
2019-05-18 19:06:44 +0200

    fix
    Signed-off-by: pokkihju <cyril.rideau@epita.fr>

--- src/filesystem/restore_save.c
+++ src/filesystem/restore_save.c
@@ -142,9 +142,9 @@ struct chained *RS_create_save_list(char *save_dir)
 {
     DIR *saves = opendir(save_dir);
     struct dirent *next;
-    char nextname[2048];
+    char nextname[4096];
     size_t k;
-    for (k = 0; *(save_dir + k); k++)
+    for (k = 0; *(save_dir + k) != '0' && k < 2048; k++)
     {
         nextname[k] = *(save_dir + k);
     }
