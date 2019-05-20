74e1d0231712ec0490592c6c57ef9d7cf61ed795
FredericCasier <frederic.casier@epita.fr>
2019-05-20 14:53:44 +0200

    update: interface.c - checkbox working
    Signed-off-by: FredericCasier <frederic.casier@epita.fr>

--- src/gui/interface.c
+++ src/gui/interface.c
@@ -11,10 +11,18 @@ GtkWidget *ask_file_load;
 GtkWidget *ask_file_restore;
 GtkWidget *ask_file_create;
 
-int encrypt;
-int compr;
-int decrypt;
-int decompr;
+GtkToggleButton *compr_toggle;
+GtkToggleButton *crypt_toggle;
+GtkToggleButton *compr_load_toggle;
+GtkToggleButton *crypt_load_toggle;
+
+GtkToggleButton *decompr_toggle;
+GtkToggleButton *decrypt_toggle;
+
+gboolean crypt_state;
+gboolean compr_state;
+gboolean decrypt_state;
+gboolean decompr_state;
 
 int interface(int argc,char *argv[])
 {
@@ -31,6 +39,13 @@ int interface(int argc,char *argv[])
     ask_file_load = GTK_WIDGET(gtk_builder_get_object(builder, "ask_file_load"));
     ask_file_create = GTK_WIDGET(gtk_builder_get_object(builder, "ask_file_create"));
     ask_file_restore = GTK_WIDGET(gtk_builder_get_object(builder, "ask_file_restore"));
+
+    crypt_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"encrypt"));
+    compr_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"compr"));
+    crypt_load_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"encrypt_load"));
+    compr_load_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"compr_load"));
+    decrypt_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"decrypt"));
+    decompr_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"decompr"));
 
     gtk_builder_connect_signals(builder,NULL);
 
@@ -77,35 +92,68 @@ void on_load_clicked()
     gtk_widget_hide(make_backup);
 }
 
-void on_save_clicked()
+
+
+void on_save_load_clicked()
 {
-    //GtkToggleButton *decompr_toggle;
-    //GtkToggleButton *decrypt_toggle;
+    //GtkToggleButton *compr_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"encrypt"));
+    //GtkToggleButton *crypt_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"compr"));
 
-    //decompr_toggle = GTK_Widget(gtk_builder_get_object(builder, "decompr"));
-    //decrypt_toggle = GTK_Widget(gtk_builder_get_object(builder, "decrypt"));
+    compr_state = gtk_toggle_button_get_active(compr_load_toggle);
+    crypt_state = gtk_toggle_button_get_active(crypt_load_toggle);
 
+    printf("TODO : Lauch Save\n");
 
-    printf("You clicked on save\n");
-    //gboolean decrypt_state;
-    //gboolean decompr_state;
-
-    //decompr_state = gtk_toggle_button_get_active(decompr_toggle);
-    //decrypt_state = gtk_toggle_button_get_active(decrypt_toggle);
-
-    //if(decompr_state){decompr=1;}
-    //else{decompr=0;}
-
-    //if(decrypt_state){decrypt=1;}
-    //else{decrypt=0;}
+    if(compr_state)
+    {
+        printf("TODO : Lauch Compression\n");
+    }
+    if(crypt_state)
+    {
+        printf("TODO : Lauch Encryption\n");
+    }
 }
 
 void on_save_create_clicked()
 {
-    printf("You clicked on save1\n");
+    //GtkToggleButton *compr_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"encrypt"));
+    //GtkToggleButton *crypt_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"compr"));
+
+        
+    compr_state = gtk_toggle_button_get_active(compr_toggle);
+    crypt_state = gtk_toggle_button_get_active(crypt_toggle);
+
+    printf("TODO : Lauch Save\n");
+
+    if(compr_state)
+    {
+        printf("TODO : Lauch Compression\n");
+    }
+    if(crypt_state)
+    { 
+        printf("TODO : Lauch Encryption\n");
+    }
+
+    
 }
 
 void on_restore_button_clicked()
 {
-    printf("You clicked on Restore\n");
+    //GtkToggleButton *decompr_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"dencrypt"));
+    //GtkToggleButton *decrypt_toggle= GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder,"decompr"));
+        
+    decompr_state = gtk_toggle_button_get_active(decompr_toggle);
+    decrypt_state = gtk_toggle_button_get_active(decrypt_toggle);
+
+    printf("TODO : Lauch restore\n");
+
+    if(decompr_state)
+    {
+        printf("TODO : Lauch Compression\n");
+    }
+    if(decrypt_state)
+    {
+            printf("TODO : Lauch Encryption\n");
+    }
+    
 }
