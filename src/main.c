c4c39a415a6de7d3cb0d3b7e6eea8bd59ef11a7d
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-05-21 16:39:29 +0200

    fix bugs in main.
    Signed-off-by: cedric.farinazzo <cedric.farinazzo@epita.fr>

--- src/main.c
+++ src/main.c
@@ -2,31 +2,14 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
 #include <fcntl.h>
 #include <string.h>
 #include <time.h>
 
 #include "gui/interface.h"
 
-#include "encryption/rotn.h"
-#include "encryption/vigenere.h"
-
-#include "encryption/rsa/rsa.h"
-#include "encryption/rsa/genkey.h"
-#include "encryption/rsa/tools.h"
-#include "encryption/rsa/rsa_file.h"
-
-#include "encryption/aes/aes.h"
-#include "encryption/aes/aes_file.h"
-#include "encryption/aes/aes_matrix.h"
-#include "encryption/aes/aes_addroundkey.h"
-#include "encryption/aes/aes_shiftrows.h"
-#include "encryption/aes/aes_subbytes.h"
-#include "encryption/aes/aes_mixcolumns.h"
-#include "encryption/aes/hashpass.h"
-
-#include "encryption/elgamal/elgamal.h"
-#include "encryption/elgamal/genkey.h"
+#include "encryption/encryption.h"
 
 #include "compression/huffman/huffman.h"
 #include "compression/struct.h"
@@ -79,7 +62,7 @@ char *restore_clear_saves(char *save_dir, int enc, int comp)
     comp = comp;
     //TODO
     DIR *directory = opendir(save_dir);
-    dirent *next;
+    struct dirent *next;
     char tempdir[4096];
     strcpy(tempdir, save_dir);
     char *start = tempdir + strlen(save_dir);
@@ -91,7 +74,7 @@ char *restore_clear_saves(char *save_dir, int enc, int comp)
     while ((next = readdir(directory)))
     {
     }
-    return tempdir;
+    return tempdir; //tempdir is a local variable. do some cancer here pls
 }
 
 void remove_dir(char *dir)
@@ -106,16 +89,16 @@ int main(int argc, char *argv[])
     argv = argv;
     printf("Hello, welcome to PackupSystem4.\n");
     printf("Do you want to save a directory or restore it ?\n"
-            "(0 => save, 1 => restore)");
+           "(0 => save, 1 => restore)");
     if (/*save a dir*/0)
     {
         printf("You have chosen to save a directory.\n"
-            "Do you already have a save or is it the first one ?\n"
-            "(0 => no prior save, 1 => a save already exists)\n");
+               "Do you already have a save or is it the first one ?\n"
+               "(0 => no prior save, 1 => a save already exists)\n");
         printf("Please give the name of the directory you want to save:\n");
         char *dirpath;
         printf("Please give the name of the file in which you want to save it. Please note that it will\n"
-            "be lost if it already exists:\n");
+               "be lost if it already exists:\n");
         char *savepath;
         char tempfile[4096];
         strcpy(tempfile, savepath);
@@ -157,7 +140,7 @@ int main(int argc, char *argv[])
         }
         remove(tempfile);
         printf("Please say which encryption you want to use:\n"
-            "(0 => no encryption, 1 => Rotn, 2 => Vigenere, 3 => AES, 4 => RSA, 5 => Elgamal)\n");
+               "(0 => no encryption, 1 => Rotn, 2 => Vigenere, 3 => AES, 4 => RSA, 5 => Elgamal)\n");
         int enc;
         switch (enc)
         {
@@ -200,262 +183,225 @@ int main(int argc, char *argv[])
         printf("Restoration done\n");
     }
     /*
-    srand(time(NULL));
+       srand(time(NULL));
 
-    if (argc == 1)
-        return interface(argc, argv);
+       if (argc == 1)
+       return interface(argc, argv);
 
-    if (argc == 2)
+       if (argc == 2)
+       {
+       if (strcmp("--version", argv[1]) == 0)
+       printf("Packup System 4 by PS4 %s v%s %s\n", TYPE, VERSION, DATE);
+
+       if (strcmp("backup", argv[1]) == 0)
+       {
+
+       struct meta_tree *tree = FILESYSTEM_build_metatree("./testfiles/content");
+       print_tree(tree->son, 0);
+       FILESYSTEM_create_save("./testfiles/content", "./testfiles/saves/save.rdtgs");
+       FILESYSTEM_free_metatree(tree);
+       }
+
+       if (strcmp("restore", argv[1]) == 0)
+       {
+       FILESYSTEM_restore_original_save("./testfiles/saves/save.rdtgs");
+
+       struct meta_tree *tree = FILESYSTEM_build_metatree("./testfiles/content");
+       print_tree(tree->son, 0);
+       FILESYSTEM_free_metatree(tree);
+       }
+
+       }
+
+       if (argc == 3)
+       {
+       if (strcmp("huffman", argv[1]) == 0)
+       {
+       int len_a = strlen(argv[2]);
+       unsigned char *text = (unsigned char *)(argv[2]);
+       printf("Text input = %s\n", text);
+       printf("Compressing...\n");
+       struct huff_out *compressed = compression(text, len_a);
+       printf("\nText ouput = %s | ", compressed->dataOUT);
+       print_chare(compressed->dataOUT, compressed->len);
+       printf("Ratio : %d%%\n", ((compressed->len * 100) / len_a));
+       free_out(compressed);
+       }
+
+       if (strcmp("dehuffman", argv[1]) == 0)
+       {
+       unsigned char *text = (unsigned char *)(argv[2]);
+       int len = strlen(argv[2]);
+       struct huff_out *compressed = compression(text, len);
+
+       struct huff_out *final = decompression(compressed->dataOUT,
+       compressed->len);
+       int len_a = strlen((char *)final->dataOUT);
+       if (len_a != len)
+       printf("Longeur differente : %d -> %d\n", len, len_a);
+       printf("Text input : %s\n", text);
+       printf("Text output : ");
+       print_chare(final->dataOUT, final->len);
+       for (int i = 0; i < final->len; ++i)
+       printf("%d", final->dataOUT[i]);
+       printf("\n");
+       free(compressed->dataOUT);
+       free(compressed);
+       free(final->dataOUT);
+       free(final);
+       }
+
+       if (strcmp("filehuffman", argv[1]) == 0)
+       {
+    char *path_output = compression_function(argv[2]);
+    long int ratio =(findSize(path_output)*100)/findSize(argv[2]);
+    printf("Input_size = %ld\n", findSize(argv[2]));
+    printf("Output_size = %ld\n", findSize(path_output));
+    printf("Ratio = %ld %%\n", ratio);
+    printf("Location compress file : %s\n", path_output);
+    free(path_output);
+}
+
+if (strcmp("filedehuffman", argv[1]) == 0)
+{
+    char *path_output = decompression_function(argv[2]);
+    printf("Location decompress file : %s\n", path_output);
+    free(path_output);
+}
+
+if (strcmp("filesystem", argv[1]) == 0)
+{
+    char *path = argv[2];
+
+    struct meta_tree *tree = FILESYSTEM_build_metatree(path);
+    printf("Scanning directory %s ...\n\n", path);
+    printf("TREE:\n");
+    print_tree(tree->son, 0);
+
+    FILESYSTEM_save_metatree(tree, "test_tree_main.txt");
+    printf("\n\nSaving tree from tree_test_main.txt\n");
+
+
+    struct meta_tree *restored = FILESYSTEM_restore_metatree("test_tree_main.txt");
+    printf("\n\nLoading tree to tree_test_main.txt\n");
+    printf("RESTORED TREE: \n");
+    print_tree(restored->son, 0);
+
+    FILESYSTEM_free_metatree(restored);
+    FILESYSTEM_free_metatree(tree);
+}
+}
+
+if (argc == 4)
+{
+    if (strcmp("rotn", argv[1]) == 0)
     {
-        if (strcmp("--version", argv[1]) == 0)
-            printf("Packup System 4 by PS4 %s v%s %s\n", TYPE, VERSION, DATE);
+        char *text = argv[2];
+        long key = atol(argv[3]);
 
-        if (strcmp("backup", argv[1]) == 0)
-        {
+        printf("text: %s   | key: %ld\n", text, key);
 
-            struct meta_tree *tree = FILESYSTEM_build_metatree("./testfiles/content");
-            print_tree(tree->son, 0);
-            FILESYSTEM_create_save("./testfiles/content", "./testfiles/saves/save.rdtgs");
-            FILESYSTEM_free_metatree(tree);
-        }
+        ROTN_encrypt(text, key);
+        printf("encrypted text: %s  |   ", text);
+        print_ascii((unsigned char*)text); printf("\n");
 
-        if (strcmp("restore", argv[1]) == 0)
-        {
-            FILESYSTEM_restore_original_save("./testfiles/saves/save.rdtgs");
+        ROTN_decrypt(text, key);
+        printf("decrypted text: %s\n", text);
+    }
 
-            struct meta_tree *tree = FILESYSTEM_build_metatree("./testfiles/content");
-            print_tree(tree->son, 0);
-            FILESYSTEM_free_metatree(tree);
-        }
+    if (strcmp("vigenere", argv[1]) == 0)
+    {
+        char *text = argv[2];
+        char *key = argv[3];
+
+        printf("text: %s   | key: %s\n", text, key);
+
+        VIGENERE_encrypt(text, key);
+        printf("encrypted text: %s  |   ", text);
+        print_ascii((unsigned char*)text); printf("\n");
+
+        VIGENERE_decrypt(text, key);
+        printf("decrypted text: %s\n", text);
+    }
+
+    if (strcmp("aes", argv[1]) == 0)
+    {
+        unsigned char *text = (unsigned char*)argv[2];
+        char *pass = argv[3];
+
+        unsigned char *key = AES_keyFromPass(pass, strlen(pass));
+        printf("password (%ld): %s\n", strlen(pass), pass);
+        printf("text(%ld): %s   | key: %s\n\n", strlen((char*)text), text, key);
+
+        unsigned char *output = NULL;
+        unsigned char *decrypt = NULL;
+
+        struct AES_ctx *ctx = AES_init(key, strlen((char*)key));
+
+        size_t outlen = AES_encrypt(ctx, text, strlen((char*)text), &output);
+        printf("\nencrypted text (%ld): %s  |   ", outlen, output);
+        print_ascii(output); printf("\n");
+
+        size_t delen = AES_decrypt(ctx, output, outlen, &decrypt);
+        printf("\n\ndecrypted text (%ld): %s  |   ", delen, decrypt);
+        print_ascii(decrypt); printf("\n");
+
+        free(output);
+        free(decrypt);
+        AES_ctx_free(ctx);
+        free(key);
+    }
+}
+
+if (argc == 5)
+{
+    if (strcmp("aes-file-enc", argv[1]) == 0)
+    {
+        int e = AES_encrypt_file(argv[2], argv[3], argv[4]);
+        if (e < 0)
+            errx(e, "aes file enc: error ");
+    }
+
+    if (strcmp("aes-file-dec", argv[1]) == 0)
+    {
+        int e = AES_decrypt_file(argv[2], argv[3], argv[4]);
+        if (e < 0)
+            errx(e, "aes file dec: error ");
+    }
+
+    if (strcmp("gen-rsa-key", argv[1]) == 0)
+    {
+        struct RSA_pubKey *pubk;
+        struct RSA_privKey *privk;
+        unsigned long keysize = (unsigned long)atol(argv[2]);
+        RSA_generateKey(keysize, &privk, &pubk);
+
+        RSA_pubk_to_file(pubk, argv[3]);
+        RSA_privk_to_file(privk, argv[4]);
+
+        RSA_free_public_key(pubk);
+        RSA_free_private_key(privk);
 
     }
 
-    if (argc == 3)
+    if (strcmp("rsa-file-enc", argv[1]) == 0)
     {
-        if (strcmp("huffman", argv[1]) == 0)
-        {
-            int len_a = strlen(argv[2]);
-            unsigned char *text = (unsigned char *)(argv[2]);
-            printf("Text input = %s\n", text);
-            printf("Compressing...\n");
-            struct huff_out *compressed = compression(text, len_a);
-            printf("\nText ouput = %s | ", compressed->dataOUT);
-            print_chare(compressed->dataOUT, compressed->len);
-            printf("Ratio : %d%%\n", ((compressed->len * 100) / len_a));
-            free_out(compressed);
-        }
-
-        if (strcmp("dehuffman", argv[1]) == 0)
-        {
-            unsigned char *text = (unsigned char *)(argv[2]);
-            int len = strlen(argv[2]);
-            struct huff_out *compressed = compression(text, len);
-
-            struct huff_out *final = decompression(compressed->dataOUT,
-                                                   compressed->len);
-            int len_a = strlen((char *)final->dataOUT);
-            if (len_a != len)
-                printf("Longeur differente : %d -> %d\n", len, len_a);
-            printf("Text input : %s\n", text);
-            printf("Text output : ");
-            print_chare(final->dataOUT, final->len);
-            for (int i = 0; i < final->len; ++i)
-                printf("%d", final->dataOUT[i]);
-            printf("\n");
-            free(compressed->dataOUT);
-            free(compressed);
-            free(final->dataOUT);
-            free(final);
-        }
-
-        if (strcmp("filehuffman", argv[1]) == 0)
-        {
-            char *path_output = compression_function(argv[2]);
-            long int ratio =(findSize(path_output)*100)/findSize(argv[2]);
-            printf("Input_size = %ld\n", findSize(argv[2]));
-            printf("Output_size = %ld\n", findSize(path_output));
-            printf("Ratio = %ld %%\n", ratio);
-            printf("Location compress file : %s\n", path_output);
-            free(path_output);
-        }
-
-        if (strcmp("filedehuffman", argv[1]) == 0)
-        {
-            char *path_output = decompression_function(argv[2]);
-            printf("Location decompress file : %s\n", path_output);
-            free(path_output);
-        }
-
-        if (strcmp("filesystem", argv[1]) == 0)
-        {
-            char *path = argv[2];
-
-            struct meta_tree *tree = FILESYSTEM_build_metatree(path);
-            printf("Scanning directory %s ...\n\n", path);
-            printf("TREE:\n");
-            print_tree(tree->son, 0);
-
-            FILESYSTEM_save_metatree(tree, "test_tree_main.txt");
-            printf("\n\nSaving tree from tree_test_main.txt\n");
-
-
-            struct meta_tree *restored = FILESYSTEM_restore_metatree("test_tree_main.txt");
-            printf("\n\nLoading tree to tree_test_main.txt\n");
-            printf("RESTORED TREE: \n");
-            print_tree(restored->son, 0);
-
-            FILESYSTEM_free_metatree(restored);
-            FILESYSTEM_free_metatree(tree);
-        }
+        struct RSA_pubKey *pub = RSA_pubKey_from_file(argv[4]);
+        int e = RSA_encode_file(argv[2], argv[3], pub);
+        RSA_free_public_key(pub);
+        if (e < 0)
+            errx(e, "rsa file enc: error ");
     }
 
-    if (argc == 4)
+    if (strcmp("rsa-file-dec", argv[1]) == 0)
     {
-        if (strcmp("rotn", argv[1]) == 0)
-        {
-            char *text = argv[2];
-            long key = atol(argv[3]);
+        struct RSA_privKey *priv = RSA_privKey_from_file(argv[4]);
+        int e = RSA_decode_file(argv[2], argv[3], priv);
+        RSA_free_private_key(priv);
+        if (e < 0)
+            errx(e, "aes file dec: error ");
+    }
 
-            printf("text: %s   | key: %ld\n", text, key);
-
-            ROTN_encrypt(text, key);
-            printf("encrypted text: %s  |   ", text);
-            print_ascii((unsigned char*)text); printf("\n");
-
-            ROTN_decrypt(text, key);
-            printf("decrypted text: %s\n", text);
-        }
-
-        if (strcmp("vigenere", argv[1]) == 0)
-        {
-            char *text = argv[2];
-            char *key = argv[3];
-
-            printf("text: %s   | key: %s\n", text, key);
-
-            VIGENERE_encrypt(text, key);
-            printf("encrypted text: %s  |   ", text);
-            print_ascii((unsigned char*)text); printf("\n");
-
-            VIGENERE_decrypt(text, key);
-            printf("decrypted text: %s\n", text);
-        }
-
-        if (strcmp("aes", argv[1]) == 0)
-        {
-            unsigned char *text = (unsigned char*)argv[2];
-            char *pass = argv[3];
-
-            unsigned char *key = AES_keyFromPass(pass, strlen(pass));
-            printf("password (%ld): %s\n", strlen(pass), pass);
-            printf("text(%ld): %s   | key: %s\n\n", strlen((char*)text), text, key);
-
-            unsigned char *output = NULL;
-            unsigned char *decrypt = NULL;
-
-            struct AES_ctx *ctx = AES_init(key, strlen((char*)key));
-
-            size_t outlen = AES_encrypt(ctx, text, strlen((char*)text), &output);
-            printf("\nencrypted text (%ld): %s  |   ", outlen, output);
-            print_ascii(output); printf("\n");
-
-            size_t delen = AES_decrypt(ctx, output, outlen, &decrypt);
-            printf("\n\ndecrypted text (%ld): %s  |   ", delen, decrypt);
-            print_ascii(decrypt); printf("\n");
-
-            free(output);
-            free(decrypt);
-            AES_ctx_free(ctx);
-            free(key);
-        }
-
-        if (strcmp("rsa", argv[1]) == 0)
-        {/*
-            char *text = argv[2];
-            size_t lentext = strlen(text);
-
-            unsigned long keysize = (unsigned long)atol(argv[3]);
-
-            printf("text: %s (size: %ld) | keysize: %ld\n\n", text, lentext, keysize);
-
-            struct RSA_pubKey *pubk;
-            struct RSA_privKey *privk;
-
-            RSA_generateKey(keysize, &privk, &pubk);
-            printf("public: n = "); mpz_out_str(stdout,10, *(pubk->n));
-            printf("   e = "); mpz_out_str(stdout, 10, *(pubk->e));
-
-            printf("\nprivate: n = "); mpz_out_str(stdout, 10, *(privk->n));
-            printf("   d = "); mpz_out_str(stdout, 10, *(privk->d)); printf("\n");
-
-            size_t elen;
-            unsigned char *encrypt = RSA_encode(pubk, (unsigned char*)text, lentext, &elen);
-            printf("\nencode (%ld): %s\n", elen, encrypt);
-
-            size_t dlen;
-            unsigned char *decode = RSA_decode(privk, encrypt, elen, &dlen);
-
-            printf("\n\ndecode text (%ld): %s\n", dlen, decode);
-
-            free(encrypt);
-            free(decode);
-
-            RSA_free_public_key(pubk);
-            RSA_free_private_key(privk);
-            */
-        }
-
-            }
-
-    if (argc == 5)
-    {
-        if (strcmp("aes-file-enc", argv[1]) == 0)
-        {
-            int e = AES_encrypt_file(argv[2], argv[3], argv[4]);
-            if (e < 0)
-                errx(e, "aes file enc: error ");
-        }
-
-        if (strcmp("aes-file-dec", argv[1]) == 0)
-        {
-            int e = AES_decrypt_file(argv[2], argv[3], argv[4]);
-            if (e < 0)
-                errx(e, "aes file dec: error ");
-        }
-        
-        if (strcmp("gen-rsa-key", argv[1]) == 0)
-        {
-            struct RSA_pubKey *pubk;
-            struct RSA_privKey *privk;
-            unsigned long keysize = (unsigned long)atol(argv[2]);
-            RSA_generateKey(keysize, &privk, &pubk);
-        
-            RSA_pubk_to_file(pubk, argv[3]);
-            RSA_privk_to_file(privk, argv[4]);
-
-            RSA_free_public_key(pubk);
-            RSA_free_private_key(privk);
-            
-        }
-
-        if (strcmp("rsa-file-enc", argv[1]) == 0)
-        {
-            struct RSA_pubKey *pub = RSA_pubKey_from_file(argv[4]);
-            int e = RSA_encode_file(argv[2], argv[3], pub);
-            RSA_free_public_key(pub);
-            if (e < 0)
-                errx(e, "rsa file enc: error ");
-        }
-
-        if (strcmp("rsa-file-dec", argv[1]) == 0)
-        {
-            struct RSA_privKey *priv = RSA_privKey_from_file(argv[4]);
-            int e = RSA_decode_file(argv[2], argv[3], priv);
-            RSA_free_private_key(priv);
-            if (e < 0)
-                errx(e, "aes file dec: error ");
-        }
-        
-    }*/
-    return EXIT_SUCCESS;
+}*/
+return EXIT_SUCCESS;
 }
